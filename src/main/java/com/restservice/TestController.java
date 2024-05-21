a6dbfab042cdcb1a30a05573ce238adbe114817e
Adam Nightingale <leo.victoro@gmail.com>
2024-05-21 16:31:23 -0300

    feat: functional demo controller

---
+++ src/main/java/com/restservice/TestController.java
@@ -0,0 +1,20 @@
+package com.restservice;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PokeController {
+
+    private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+    
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "world") String name) {
+        System.out.println(name);
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+}
