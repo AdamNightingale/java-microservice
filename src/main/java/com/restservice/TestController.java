9313fc5900b5cf1df6ac0d0c3c772d0ec1cd3c85
Adam Nightingale <leo.victoro@gmail.com>
2024-05-21 16:35:23 -0300

    fix: class name

--- src/main/java/com/restservice/TestController.java
+++ src/main/java/com/restservice/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class PokeController {
+public class TestController {
 
     private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
