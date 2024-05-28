b5ac98619aebeebdc80b7d1cbf11c4bbf550a0dc
Adam Nightingale <leo.victoro@gmail.com>
2024-05-28 16:33:10 -0300

    feat: created pokemon class

---
+++ src/main/java/com/restservice/pokemon/Pokemon.java
@@ -0,0 +1,3 @@
+package com.restservice.pokemon;
+
+public record Pokemon(long id, String name, Types[] types) {}
---
+++ src/main/java/com/restservice/pokemon/Types.java
@@ -0,0 +1,3 @@
+package com.restservice.pokemon;
+
+public record Types(Type type) {}
